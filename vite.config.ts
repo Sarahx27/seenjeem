6fc12ef218f294410be7ddd47c18c582ca9c5a20
Sarah <Sarahx27@users.noreply.github.com>
2025-11-02 16:05:03 +0300

    first commit'

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  base: "/seenjeem/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
-}));
+});
